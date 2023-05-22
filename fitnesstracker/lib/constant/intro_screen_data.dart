d10872025c87e72d98fb4ab559e13fb611ab4e12
Semir Hamid <semhamirid@gmail.com>
2023-05-23 02:42:29 +0300

    initializing the frontend and backend of the fitness tracking app

---
+++ fitnesstracker/lib/constant/intro_screen_data.dart
@@ -0,0 +1,32 @@
+import 'package:fitnesstracker/models/intro_screen_model.dart';
+
+List<IntroScreenItemData> introScreenData = [
+  const IntroScreenItemData(
+    title: "Track Your Exercise",
+    subtitle:
+        "Monitor your daily activity and stay on top of your fitness goals.",
+    image: "assets/images/intro_image_1.jfif",
+  ),
+  const IntroScreenItemData(
+    title: "Manage Your Diet",
+    subtitle: "Track your calorie intake and follow personalized diet plans.",
+    image: "assets/images/intro_image_2.jpg",
+  ),
+  const IntroScreenItemData(
+    title: "Improve Your Sleep",
+    subtitle: "Track your sleep patterns and optimize your sleep quality.",
+    image: "assets/images/intro_image_3.jpg",
+  ),
+  const IntroScreenItemData(
+    title: "Build Stronger Workouts",
+    subtitle:
+        "Track your sets, reps, and weights for each exercise to monitor your progress.",
+    image: "assets/images/intro_image_4.jpg",
+  ),
+  const IntroScreenItemData(
+    title: "Reach Your Nutrition Goals",
+    subtitle:
+        "Discover personalized diet plans and healthy meal recommendations.",
+    image: "assets/images/intro_image_5.webp",
+  ),
+];
---
+++ fitnesstracker/lib/models/intro_screen_model.dart
@@ -0,0 +1,11 @@
+class IntroScreenItemData {
+  final String title;
+  final String subtitle;
+  final String image;
+
+  const IntroScreenItemData({
+    required this.title,
+    required this.subtitle,
+    required this.image,
+  });
+}
