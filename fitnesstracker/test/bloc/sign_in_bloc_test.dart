24f4bef1bc6bd313e633adba90533334e04d5461
Addisu Abitew <addisu1abitew2@gmail.com>
2023-06-03 20:07:55 -0300

    test

---
+++ fitnesstracker/test/bloc/sign_in_bloc_test.dart
@@ -0,0 +1,11 @@
+import 'package:fitnesstracker/domain/auth/i_auth_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+@GenerateMocks([IAuthRepository])
+void main() {
+  group('Authentication testing', () {
+    test("sign in testing", () {});
+  });
+}
