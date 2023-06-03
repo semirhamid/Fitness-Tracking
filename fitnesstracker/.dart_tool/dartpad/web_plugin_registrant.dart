07a40430866184145c708627ac52cced021c6e3d
Addisu Abitew <addisu1abitew2@gmail.cdcom>
2023-06-03 19:21:41 -0300

    builder and injections with auth

---
+++ fitnesstracker/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,24 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:connectivity_plus/src/connectivity_plus_web.dart';
+import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:flutter_keyboard_visibility_web/flutter_keyboard_visibility_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  ConnectivityPlusWebPlugin.registerWith(registrar);
+  FilePickerWeb.registerWith(registrar);
+  FlutterKeyboardVisibilityPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
